d693aa73b9b43d2be7d220f9f9f2cdf0c67410e2
Shubhamdeshmukh21 <sd1075981@gmail.com>
2025-01-29 13:32:47 +0530

    Add files via upload

---
+++ spotify clone/script.js
@@ -0,0 +1,62 @@
+
+
+
+//  async function main(){
+
+//     let a =  await fetch( "http:127.0.0.1:3000/songs/")
+//     let response = await a.text();
+//     console.log(response)
+// }
+
+// main()
+
+//  let a = 10
+
+//  let a = 2
+// console.log(a)
+
+// const a =[1,2,3,4,5]
+// a.forEach(element => {
+//     console.log(element) 
+// });
+
+
+
+// for (const i of a) {
+//     console.log(i)
+    
+// }
+// a.map((i)=>{
+//     console.log(i)
+// })
+
+// // a.filter((i)=>{
+// //     return i<6
+// // })
+// a.reduce((i)=>{
+//     console.log(i)
+// })
+
+//  const animal = {
+//    eats :true,
+//  }
+
+// const rabbit  ={
+//     hops :true
+
+// }
+
+// console.log(animal)
+// console.log(rabbit)
+
+// rabbit.__proto__ = animal
+// class animal{
+//     constructor(name)
+
+//     eats =" kha rha hu"
+// }
+
+
+
+
+ 
